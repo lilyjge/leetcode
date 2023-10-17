8cf3ce17bd5d17fe557d376f2cc5aa6d05d275f0
lilyjge <lilyjge@gmail.com>
2023-10-17 14:21:14 +0000

    [LeetCode Sync] Runtime - 5 ms (0.90%), Memory - 9.5 MB (99.98%)

---
+++ 0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp;
+        for(int i = 0, j = 0; i < m || j < n;){
+            if(i < m && j < n){
+                if(nums1[i] < nums2[j]){
+                    temp.push_back(nums1[i]);
+                    i++;
+                }
+                else{
+                    temp.push_back(nums2[j]);
+                    j++;
+                }
+            }
+            else if(i < m){
+                temp.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                temp.push_back(nums2[j]);
+                j++;
+            }
+        }
+        nums1 = temp;
+    }
+};
